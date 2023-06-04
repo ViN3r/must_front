98d025d2ea933085a961e3078bb9575db46478fe
Ervin <ervin.milandou@gmail.com>
2023-06-04 15:14:29 +0200

    Ajout routing, ajout component navbar et login

--- must/src/app/app.module.ts
+++ must/src/app/app.module.ts
@@ -2,13 +2,23 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavBarComponent } from './component/nav-bar/nav-bar.component';
+import { RouterModule } from '@angular/router';
+import { LoginComponent } from './component/login/login.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    RouterModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
